b46d1f52a9e778ab2cd8c5338b1f04835d73829e
wagjiake <wangjiake@hzzl.net>
2020-10-12 14:08:08 +0800

    dev push

---
+++ src/main/java/com/demo/disruptor/MyClass.java
@@ -0,0 +1,19 @@
+package com.demo.disruptor;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class MyClass implements Externalizable
+{
+    public void writeExternal(ObjectOutput out) throws IOException
+    {
+
+    }
+
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException
+    {
+
+    }
+}
--- src/main/java/com/demo/disruptor/test01.java
+++ src/main/java/com/demo/disruptor/test01.java
@@ -1,9 +1,16 @@
 package com.demo.disruptor;
 
+import java.util.Collections;
+import java.util.List;
+
 public class test01
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws RuntimeException
     {
+            List<String> l = Collections.emptyList();
+            l.add("1");
+        String s = l.get(0);
+        System.out.println(s);
 
     }
 }
